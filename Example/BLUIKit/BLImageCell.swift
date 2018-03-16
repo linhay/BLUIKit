2ceb737fbc3b335e1276f2a02229c963bb320ab5
linhey <158179948@qq.com>
2018-03-16 15:57:03 +0800

    新增BLImageView提供gif功能

---
+++ Example/BLUIKit/BLImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  BLImageCell.swift
+//  BLUIKit_Example
+//
+//  Created by linhey on 2018/3/16.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import BLUIKit
+
+class BLImageCell: UITableViewCell {
+
+  @IBOutlet weak var icon: BLImageView!
+  
+    
+}
